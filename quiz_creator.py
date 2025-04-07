b723e9f2832f0c6f7d4c2eee13f026fd967e01aa
unknown <ramosyazmien2@gmail.com>
2025-04-08 04:41:33 +0800

    pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,8 @@
+# pseudocode
+# ask user for question
+# ask user for 4 possible answers (a, b, c, d)
+# ask user for the correct answer (a, b, c, d)
+# store questions, choices, and correct answer in a list
+# loop until the user chooses to exit 
+# save all questions in a text file
+# initialize the score
